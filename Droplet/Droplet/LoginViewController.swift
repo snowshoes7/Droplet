3e4508225602aed95d1dc995bd61873fa5347b45
Owen Thompson <owen.thompson21@flhsemail.org>
2020-03-27 10:20:30 -0700

    Add login controller and app icon

---
+++ Droplet/Droplet/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Droplet
+//
+//  Created by Owen Thompson on 3/27/20.
+//  Copyright © 2020 DropTeam. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
