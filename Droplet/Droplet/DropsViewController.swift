fe518b5ef308a603dd6e58333269d87ec76ef985
Owen Thompson <owen.thompson21@flhsemail.org>
2020-03-30 15:32:33 -0700

    add settings screen

--- Droplet/Droplet/DropsViewController.swift
+++ Droplet/Droplet/DropsViewController.swift
@@ -10,21 +10,12 @@ import UIKit
 
 class DropsViewController: UIViewController {
 
+    @IBOutlet weak var btnSettings: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func actionSettings(_ sender: Any) {
     }
-    */
-
 }
